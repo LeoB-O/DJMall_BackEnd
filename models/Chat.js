9afe6a63d19b30ad8500ba2e4995ae47462b521d
leob_o <liubobotb@126.com>
2019-01-12 16:35:59 +0800

    add: database for chat

---
+++ models/Chat.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+let chatSchema = mongoose.Schema({
+    from: String,
+    to: String,
+    contents: [{
+        time: Date,
+        message: String
+    }],
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
+});
+
+let Chat = mongoose.model('Chat', chatSchema);
+
+module.exports = Chat;
