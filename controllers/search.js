8a415216d8874e76f767efd0711bc54068f9e19e
leob_o <liubobotb@126.com>
2019-01-12 23:35:49 +0800

    fix: search api

--- controllers/search.js
+++ controllers/search.js
@@ -4,13 +4,10 @@ const util = require('../util/util');
 module.exports={
     search:async function(req,res,next){
         let searchinput=req.body.searchinput
-        var query={}
-        var query1={}
-        var query2={}
-        query1['eName']=new RegExp(searchinput)
-        query2['pinyin']=new RegExp(searchinput)
-        let goods=await Good.find().or([query1,query2])
-        console.log(goods)
+        let query={name: new RegExp(searchinput)}
+        let query1={eName: new RegExp(searchinput)}
+        let query2={pinyin: new RegExp(searchinput)}
+        let goods=await Good.find().or([query,query1,query2]).exec()
         goods=goods.map((current)=>{
             return {
                 id:current._id,
