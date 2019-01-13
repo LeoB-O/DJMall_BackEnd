0c1c7324b5c7e6581b430db9c67aef05d9a9ba31
Zeo <yangcrazy30@hotmail.com>
2019-01-13 18:31:06 +0800

    add api about rate

---
+++ controllers/rate.js
@@ -0,0 +1,35 @@
+const util = require('../util/util');
+const Good = require('../models/Good');
+
+
+
+module.exports = {
+    commitrate: async function (req, res, next) {
+        let goodid=req.body.goodid
+        let crate=Number.parseInt(req.body.crate)
+        let rate=await Good.findOne({
+            _id:goodid
+        });
+        
+        let goodrate=rate.Rate.rate
+        let num=rate.Rate.ratenum
+        let total=goodrate*num+crate
+        console.log('total'+total)
+        let current=total/(num+1)
+        console.log('current '+current)
+        // let current=(goodrate*num+crate)/(num+1)
+        rate.Rate.rate=current
+        rate.Rate.ratenum=num+1
+        rate.save()
+        
+        util.handleResponse(res,null,{})
+    },
+    getrate: async function (req, res, next) {
+        let goodid=req.query.goodid
+        let rate=await Good.findOne({
+            _id:goodid
+        });
+        let goodrate=rate.Rate.rate;
+        util.handleResponse(res,null,{goodrate:goodrate})
+    }
+}
