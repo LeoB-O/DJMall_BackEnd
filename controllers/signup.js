ef0d7b07c7485e2c768a551663447fda4d76ca59
Zeo <yangcrazy30@hotmail.com>
2019-01-15 17:03:43 +0800

    complete signup

--- controllers/signup.js
+++ controllers/signup.js
@@ -6,11 +6,12 @@ module.exports = {
         let username=req.body.username
         let email=req.body.email
         let password=req.body.email
+        let permission=req.body.permission
         User.findOne().or([{username:username},{email:email}]).exec(function(err,user){
             if(user==null)
             {
                 Util.handleResponse(res, err, {ok:true})
-                User.create({username:username,email:email,password:password,permission:1})
+                User.create({username:username,email:email,password:password,permission:permission})
             }
             else
             {
